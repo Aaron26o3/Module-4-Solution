c988f4bee0db21cd3574e6ec1ddfa5454542b87a
Aaron26o3 <paratesarang3@gmail.com>
2022-01-14 19:34:13 +0530

    V.1

---
+++ script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="Jhon";
+names[2]="jen";
+names[3]="jason";
+names[4]="Paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="lara";
+names[8]="Jim";
+names[9]="yono";
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'||names[i].charAt(0)==='j') {
+		console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hellow "+ names[i])
+	}
+}
